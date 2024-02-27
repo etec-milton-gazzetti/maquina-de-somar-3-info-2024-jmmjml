5987d4bbfda1f991700a33dc2bf8720cdc2cae61
jmmjml <116905479+jmmjml@users.noreply.github.com>
2024-02-27 12:22:04 +0000

    secondcommit

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ETEC Professor Milton Gazzeti</Text>
+      <Text  style={{fontStyle: 'italic',}}>Terceira série</Text>
+      <Text  style={{textDecorationLine: 'line-through',}}>MTEC Informática para Internet</Text>
       <StatusBar style="auto" />
     </View>
   );
